41001aef1c382d810406836f7c7cc0c9e881556d
Evgeny Skorokhodov <skorokhodov_2016@inbox.ru>
2022-01-09 19:53:22 +0300

    Added the TestSystem

--- src/ru/project/RunAPP.java
+++ src/ru/project/RunAPP.java
@@ -3,6 +3,9 @@ package ru.project;
 public class RunAPP {
 
     public static void main(String[] args) {
-        
+
+        TestSystem testSystem =  new TestSystem();
+        System.out.println(testSystem);
+
     }
 }
---
+++ src/ru/project/TestSystem.java
@@ -0,0 +1,5 @@
+package ru.project;
+
+public class TestSystem {
+
+}
