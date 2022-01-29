991adff2dbbb4f46497ba887693cb2c3591fc759
pashalan <pashalan@mail.ru>
2022-01-29 13:06:23 +0300

    Добавил еще один объект car2

--- src/ru/project/Main.java
+++ src/ru/project/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
     Car car = new Car("грузовик", "Скания", "синяя");
     System.out.println("У нас есть " + car.getType() + " модели " + car.getModel() + " цвета " + car.getColor());
+    Car car2 = new Car("автобус", "Huandai", "белый");
+    System.out.println("У нас есть " + car2.getType() + " модели " + car2.getModel() + " цвета " + car2.getColor());
 
 
 
